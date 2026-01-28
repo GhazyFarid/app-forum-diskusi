fcdaa9bb70ae138b13ead36ca3d30241dad21a8e
FaridGhazy <farid.bhp9431@gmail.com>
2026-01-28 09:22:14 +0700

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -49,7 +49,7 @@ export default function LoginPage() {
           }}
         >
           <BiArrowBack size={18} />
-          Kembalii
+          Kembali
         </button>
         <h2>Welcome Back</h2>
 
